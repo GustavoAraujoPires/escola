bcbdd21e99f6c37b643a8b2d328ac979d1005757
Gustavo Araujo <gustavoaraujopires9@gmail.com>
2025-08-13 16:50:24 -0300

    Aula Java 13/08/2025

---
+++ src/main/java/com/etec/escola/Models/Aluno.java
@@ -0,0 +1,52 @@
+package com.etec.escola.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private  Long Id;
+    private String name;
+    private String telefone;
+    private String email;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
